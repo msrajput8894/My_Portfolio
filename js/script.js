9513d39bd5485d4a239ce9d7619eeaf71902eca3
Mahendra Rajput <akashrajput1434@gmail.com>
2023-07-12 11:45:21 +0530

    added sticky navigation to our webpage using javascript and css

--- js/script.js
+++ js/script.js
@@ -19,3 +19,33 @@ btnNavEl.addEventListener('click', function(){
   headerEl.classList.toggle("nav-open")
 
 });
+
+
+//working sticky navigation
+
+
+const sectionHomeEl = document.querySelector(".section-Home");
+
+const obs = new IntersectionObserver(function(entries){
+  const ent = entries[0];
+  console.log(ent);
+  if(ent.isIntersecting === false){
+
+    document.body.classList.add('sticky');
+  }
+  if(ent.isIntersecting === true){
+
+    document.body.classList.remove('sticky');
+  }
+
+  
+},
+{
+
+  //in the viewport
+  root: null,
+  threshold:0,
+  rootMargin: '-80px',
+
+});
+obs.observe(sectionHomeEl);
